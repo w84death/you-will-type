96cbe913200ff27399911ef3ed04a7cc5538f83e
Krzysztof Jankowski <w84death@gmail.com>
2017-09-18 17:46:11 +0200

    pre-alpha initial commit

---
+++ letter.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+export var letter = 'a'
+export var direction = 'a'
+
+var anim
+var letters_temp
+var letter_label
+
+func _ready():
+	self.anim = self.get_node('anim')
+	var r = rand_range(0, self.anim.get_current_animation_length())
+	self.anim.seek(r)
+	self.letter_label = self.get_node('sprite/label')
+	
+func write(letter):
+	self.letter_label.set_text(str(letter))
+
+func new_letter(letters):
+	var rand_letter = int(rand_range(0,letters.size()))
+	self.letter = letters[rand_letter]
+	write(self.letter)
+	letters.remove(rand_letter)
+	return letters
